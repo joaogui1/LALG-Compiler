89d2174836c538ad25582092ce344b9b9b6ecf0d
Luisa Moura <luisamoura@Luisas-MacBook-Pro.local>
2022-07-10 19:04:28 -0300

    adding comments to run

--- run.py
+++ run.py
@@ -6,18 +6,22 @@ from tokenizer import get_token
 from loader.main_io import LalgFile
 
 if __name__ == '__main__':
+    # add argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', '-i', type=str, required=True)
     parser.add_argument('--output', '-o', type=str)
     args = parser.parse_args()
     
+    # uses LalgFile to read code
     print('Reading file...')
     tokens = get_token(LalgFile(input_file=args.input))
 
+    # uses parser to parse tokens
     print('Parsing...')
     parser = Parser(tokens=tokens)
     byte_array = parser.parse()
     
+    # uses bytes to execute the code
     print('Emulating...')
     emulator = Emulator(byte_array)
     emulator.start()
